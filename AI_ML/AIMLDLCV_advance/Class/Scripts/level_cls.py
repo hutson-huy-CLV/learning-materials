537df39b016a234620de496d28ddcbc4545e05fd
Hutson Huy <huy.nguyentan.tpv@one-line.com>
2024-07-12 14:10:18 +0700

    update the code of NLP

--- AI_ML/AIMLDLCV_advance/Class/Scripts/level_cls.py
+++ AI_ML/AIMLDLCV_advance/Class/Scripts/level_cls.py
@@ -3,6 +3,13 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report
+from sklearn.feature_selection import SelectKBest, chi2, SelectPercentile
+from sklearn.ensemble import RandomForestClassifier
+from imblearn.over_sampling import RandomOverSampler, SMOTEN
 import re
 
 def filter_location(loc):
@@ -13,23 +20,57 @@ def filter_location(loc):
         return loc
 
 
-data = pd.read_excel("final_project.ods", engine="odf", dtype=str)
+data = pd.read_excel(r'Datasets\final_project.ods', engine="odf", dtype=str)
 data = data.dropna(axis=0)
 data["location"] = data["location"].apply(filter_location)
 target = "career_level"
 x = data.drop(target, axis=1)
 y = data[target]
+
+ros = SMOTEN(random_state=42, k_neighbors=2, sampling_strategy={
+    "managing_director_small_medium_company": 500,
+    "specialist": 500,
+    "director_business_unit_leader": 500,
+    "bereichsleiter": 1000
+})
+
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+print(y_train.value_counts())
+print("------------")
+x_train, y_train = ros.fit_resample(x_train, y_train)
+print(y_train.value_counts())
+exit(0)
 
 preprocessor = ColumnTransformer(transformers=[
     ("title", TfidfVectorizer(), "title"),
-    ("location", OneHotEncoder(), ["location"]),
-    ("description", TfidfVectorizer(stop_words="english"), "description"),
-    ("function", OneHotEncoder(), ["function"]),
+    ("location", OneHotEncoder(handle_unknown="ignore"), ["location"]),
+    ("description", TfidfVectorizer(stop_words="english", ngram_range=(1, 2), min_df=0.01, max_df=0.95), "description"),
+    ("function", OneHotEncoder(handle_unknown="ignore"), ["function"]),
     ("industry", TfidfVectorizer(), "industry"),
 ])
 
-processed_data = preprocessor.fit_transform(x_train)
-print(processed_data.shape)
+cls = Pipeline(steps=[
+    ("preprocessor", preprocessor),
+    ("feature_selection", SelectPercentile(chi2, percentile=5)),
+    ("model", RandomForestClassifier())
+])
+
+
+cls.fit(x_train, y_train)
+y_predict = cls.predict(x_test)
+print(classification_report(y_test, y_predict))
+#                                         precision    recall  f1-score   support
+#
+#                         bereichsleiter       0.63      0.18      0.28       185
+#          director_business_unit_leader       1.00      0.19      0.32        16
+#                    manager_team_leader       0.66      0.76      0.71       518
+# managing_director_small_medium_company       0.00      0.00      0.00         1
+#   senior_specialist_or_project_manager       0.86      0.93      0.89       889
+#                             specialist       0.00      0.00      0.00         6
+#
+#                               accuracy                           0.78      1615
+#                              macro avg       0.53      0.34      0.37      1615
+#                           weighted avg       0.77      0.78      0.75      1615
 
 
